f9e9feb2b1686e452242a1d815dd458e917620f3
George Beresford <george.e.beresford@gmail.com>
2017-01-21 20:15:18 +0000

    Added an initial commit
    Crosshair can be moved with the W, A, S and D keys.
    Ctrl + F will allow the user to browse their computer for a custom
    crosshair
    Ctrl + R will reset the crosshair back to its original image.
    Ctrl + C will center the crosshair back to the middle of their primary
    monitor.

---
+++ CentralisedCrosshair/CentralisedCrosshair/Crossshair.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+
+namespace CentralisedCrosshair
+{
+    partial class Crossshair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Crossshair));
+            this.SuspendLayout();
+            // 
+            // Crossshair
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Crossshair";
+            this.Opacity = 0.66D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Crosshair Overlay";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Crossshair_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OnKeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.OnKeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        //public Color TransparencyColor = Color.FromArgb(0, 8, 8, 8);
+        public Color TransparencyColor = Color.FromArgb(75, 75, 75);
+
+        #endregion
+    }
+}
---
+++ CentralisedCrosshair/CentralisedCrosshair/Crossshair.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CentralisedCrosshair
+{
+    public partial class Crossshair : Form
+    {
+        public Crossshair()
+        {
+            InitializeComponent();
+        }
+
+        #region Events
+        private void Crossshair_Load(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = this.TransparencyColor;
+            this.TransparencyKey = this.TransparencyColor;
+
+            this.BackgroundImage = this.BackgroundImage = global::CentralisedCrosshair.Properties.Resources.precision;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+
+            this.ClientSize = this.BackgroundImage.Size;
+
+            centraliseWindow();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (ctrlDown)
+            {
+                Point newPosition = new Point()
+                {
+                    X = this.Location.X,
+                    Y = this.Location.Y
+                };
+
+                switch (e.KeyCode)
+                {
+                    case (Keys.W):
+                        newPosition.Y = this.Location.Y - 1;
+                        break;
+                    case (Keys.S):
+                        newPosition.Y = this.Location.Y + 1;
+                        break;
+                    case (Keys.A):
+                        newPosition.X = this.Location.X - 1;
+                        break;
+                    case (Keys.D):
+                        newPosition.X = this.Location.X + 1;
+                        break;
+                }
+
+                this.Location = newPosition;
+            }
+            else if (e.KeyCode == Keys.ControlKey)
+            {
+                ctrlDown = true;
+            }
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                ctrlDown = false;
+            }
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            int wl = GetWindowLong(this.Handle, GWL.ExStyle);
+            wl = wl | 0x80000 | 0x20;
+            SetWindowLong(this.Handle, GWL.ExStyle, wl);
+
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (ctrlDown)
+            {
+                switch (e.KeyChar)
+                {
+                    // 'C'
+                    case ('\u0003'):
+                        centraliseWindow();
+                        break;
+
+                    // 'F'
+                    case ('\u0006'):
+                        OpenFileDialog fileDialog = new OpenFileDialog();
+                        fileDialog.Filter = "Portable Network Graphics (*.png)|*.PNG";
+
+                        if (fileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            this.BackgroundImage = Image.FromFile(fileDialog.FileName);
+                            this.Size = this.BackgroundImage.Size;
+                            this.TopMost = true;
+                        }
+
+                        break;
+
+                    // 'R'
+                    case ('\u0012'):
+                        this.BackgroundImage = global::CentralisedCrosshair.Properties.Resources.precision;
+                        this.Size = this.BackgroundImage.Size;
+                        this.TopMost = true;
+                        break;
+
+                }
+            }
+        }
+        #endregion
+
+        #region Enums
+        public enum GWL
+        {
+            ExStyle = -20
+        }
+
+        public enum WS_EX
+        {
+            Transparent = 0x20,
+            Layered = 0x80000
+        }
+
+        public enum LWA
+        {
+            ColorKey = 0x1,
+            Alpha = 0x2
+        }
+        #endregion
+
+        #region DLL Imports
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        public static extern int SetWindowLong(IntPtr hWnd, GWL nIndex, int dwNewLong);
+        #endregion
+
+        #region Private Methods
+        private void centraliseWindow()
+        {
+            Screen mainScreen = Screen.PrimaryScreen;
+
+            this.SetBounds(Convert.ToInt32(mainScreen.Bounds.Width / 2 - this.BackgroundImage.Width / 2),
+                Convert.ToInt32(mainScreen.Bounds.Height / 2 - this.BackgroundImage.Height / 2),
+                this.BackgroundImage.Width,
+                this.BackgroundImage.Height);
+        }
+        #endregion
+
+        #region Properties
+        private bool ctrlDown = false;
+        #endregion
+    }
+}
